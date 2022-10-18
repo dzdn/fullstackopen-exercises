6abf10d62b903b59bb2b24b07f416dd5bd37236b
dzdn <jdustinwilliams@gmail.com>
2022-10-18 19:53:44 -0400

    Complete 2.8 (add number field).

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,11 +4,12 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: 1 }
+    { name: 'Arto Hellas', number: "123-456-7890", id: 1 }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const handlePersonChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
   
@@ -20,10 +21,16 @@ const App = () => {
     }
     const personObject = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -31,7 +38,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handlePersonChange} />
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
